54bf4fd69a995180e2794bb0ff2b85f454de8304
Peter Stanko <wermington@gmail.com>
2017-11-30 14:49:51 +0100

    Fixes

--- specialisation.cpp
+++ specialisation.cpp
@@ -7,16 +7,19 @@
 using namespace std;
 
 template<typename T>
-struct Type
-{
-    const T & val;
-    std::ostream& operator() (std::ostream& out) const { out << "T - unknown type"; return out; }
-};
+struct Type;
 
 template<typename T>
 Type<std::remove_cv_t<std::remove_reference_t<T>>> get_info(const T & value) {
     return { value };
 }
+
+template<typename T>
+struct Type
+{
+    const T & val;
+    std::ostream& operator() (std::ostream& out) const { out << "T - unknown type"; return out; }
+};
 
 template<>
 struct Type<std::string>
@@ -32,6 +35,13 @@ struct Type<std::vector<V>>
     std::ostream& operator() (std::ostream& out) const { V instance;  out << "T - vector type of (" << get_info(instance) << ")"; return out; }
 };
 
+template<typename T, size_t S>
+struct Type<T[S]>
+{
+    const T (&val)[S];
+    std::ostream& operator() (std::ostream& out) const { out << "T - c array type of (" << get_info<T>({}) << ")"; return out; }
+};
+
 template<>
 struct Type<int>
 {
@@ -45,17 +55,24 @@ std::ostream& operator<<(std::ostream& os, Type<T>&& val) {
 	return val(os);
 }
 
-
-
 int main()
 {
     cout << get_info(std::vector<int>()) << endl;
-    cout << get_info(std::string("Ahoj svet!")) << endl;
-    cout << get_info("Ahoj svet!") << endl;
 
     cout << get_info(10) << endl;
     cout << get_info(10U) << endl;
     cout << get_info(10L) << endl;
     cout << get_info(10.0) << endl;
+
+    /* Problems with const char*/
+
+    cout << "\n\nStrings:\n";
+    cout << get_info(std::string("Ahoj svet!")) << endl;
+    const char *hello = "Ahoj svet!";
+    cout << get_info(hello) << endl;
+    cout << get_info("Ahoj svet") << endl;
+
+    char ahoj[] = "C Ahoj svet!";
+    cout << get_info(ahoj) << endl;
     return 0;
 }
